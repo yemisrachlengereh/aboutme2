ee375df2d922f6127c085540e6aeb66e5f1d4bc4
Ratna <me.ratna.maharjan@gmail.com>
2017-10-02 17:17:43 -0400

    Initial commit

---
+++ src/aboutme.java
@@ -0,0 +1,12 @@
+public class aboutme {
+    public static void main(String arg[])
+    {
+    System.out.println("my name is yemiserach lengereh");
+    System.out.println("am 21 years old");
+    System.out.println("i am beginner for java course");
+    System.out.println("intersting about my self i love people and am socable");
+    System.out.println("i am from ethiopia");
+    }
+}
+
+
